815ceb5a09ad23974b6bf332f9b41a7b5edc21bd
Yoshiki Hiraba <hiraba09@gmail.com>
2018-01-22 00:16:56 +0900

    first commit

---
+++ test/fizz_buzz_test.rb
@@ -0,0 +1,27 @@
+# minitestでテストを記述した場合
+require 'minitest/autorun'
+require_relative '../lib/fizz_buzz.rb'
+
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal '4', fizz_buzz(4)
+    assert_equal 'Buzz', fizz_buzz(5)
+    assert_equal 'Fizz', fizz_buzz(6)
+    assert_equal 'Fizz Buzz', fizz_buzz(15)
+  end
+end
+
+# RSpecでテストを記述した場合
+require './lib/fizz_buzz'
+
+RSpec.describe 'Fizz Buzz' do
+  example 'fizz_buzz' do
+    expect(fizz_buzz(1)).to eq '1'
+    expect(fizz_buzz(2)).to eq '2'
+    expect(fizz_buzz(3)).to eq 'Fizz'
+    # ...
+  end
+end
